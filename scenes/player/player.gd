b0c60dffd2e1bbc17f239e07dae6885a875a71fa
Matt Rosanio <matt.rosanio@gmail.com>
2024-01-14 15:46:24 -0500

    Add player and movement, set up tilemap

---
+++ scenes/player/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const SPEED = 2;
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up");
+	var direction = Vector2(x_movement, y_movement).normalized()
+	velocity = SPEED * direction
+	move_and_collide(velocity)
