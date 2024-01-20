4289b0bebc55c9c35d40041f2177acd9d07d7839
Matt Rosanio <matt.rosanio@gmail.com>
2024-01-20 15:08:20 -0500

    Update player movement, add house and NPC assets

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 2;
+const SPEED = 80;
 
 func _ready():
 	pass
@@ -11,4 +11,5 @@ func _process(delta):
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up");
 	var direction = Vector2(x_movement, y_movement).normalized()
 	velocity = SPEED * direction
-	move_and_collide(velocity)
+	
+	move_and_slide()
