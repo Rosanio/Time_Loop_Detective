3ea4318afe8cfe49746a5a234598a4bd4dc1b254
Matt Rosanio <matt.rosanio@gmail.com>
2025-10-23 21:30:46 -0400

    Import different schedules for each npc

--- scenes/game_object/npc/npc.gd
+++ scenes/game_object/npc/npc.gd
@@ -1,27 +1,10 @@
 extends Area2D
 
 const SPEED = 0.5
-const SCHEDULE = [
-	{
-		"time": {
-			"hour": 9,
-			"minute": 0,
-			"am_pm": "AM"
-		},
-		"coords": Vector2i(51, 26)
-	},
-	{
-		"time": {
-			"hour": 0,
-			"minute": 3,
-			"am_pm": "PM"
-		},
-		"coords": Vector2i(248, 407)
-	}
-]
 
 @export var dialog_context: DialogContextResolver
 @export var sprite_texture: Texture2D
+@export_file("*.json") var schedule_json_path: String
 
 @onready var sprite = $Sprite2D
 @onready var tile_map = $"../TileMap"
@@ -30,26 +13,13 @@ const SCHEDULE = [
 var astar_grid: AStarGrid2D
 var current_id_path: Array[Vector2i]
 
+var schedule: Array
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite.texture = sprite_texture
-	astar_grid = AStarGrid2D.new()
-	astar_grid.region = tile_map.get_used_rect()
-	astar_grid.cell_size = Vector2i(16, 16)
-	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
-	astar_grid.update()
-	
-	for x in tile_map.get_used_rect().size.x:
-		for y in tile_map.get_used_rect().size.y:
-			var tile_position = Vector2i(
-				x + tile_map.get_used_rect().position.x,
-				y + tile_map.get_used_rect().position.y
-			)
-			
-			var tile_data = tile_map.get_cell_tile_data(tile_position)
-			
-			if tile_data == null or tile_data.get_custom_data("walkable") == false:
-				astar_grid.set_point_solid(tile_position)
+	initialize_pathfinding()
+	load_schedule()
 	
 	world_time_manager.on_tick.connect(on_world_time_tick)
 	($InteractableComponent as Interactable).interact.connect(on_interact)
@@ -71,9 +41,41 @@ func on_interact():
 
 
 func on_world_time_tick(time):
-	for event in SCHEDULE:
+	for event in schedule:
 		if is_time_equal(time, event["time"]):
 			move_to(event["coords"])
+
+
+func initialize_pathfinding():
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tile_map.get_used_rect()
+	astar_grid.cell_size = Vector2i(16, 16)
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar_grid.update()
+
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x,
+				y + tile_map.get_used_rect().position.y
+			)
+
+			var tile_data = tile_map.get_cell_tile_data(tile_position)
+
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
+
+
+func load_schedule():
+	var file := FileAccess.open(schedule_json_path, FileAccess.READ)
+	if file:
+		var text := file.get_as_text()
+		var unformatted_schedule = JSON.parse_string(text)
+		for event in unformatted_schedule["schedule"]:
+			var x = event["coords"][0]
+			var y = event["coords"][1]
+			event["coords"] = Vector2i(x, y)
+		schedule = unformatted_schedule["schedule"]
 
 
 func is_time_equal(time1, time2):
@@ -83,7 +85,7 @@ func is_time_equal(time1, time2):
 func move_to(destination: Vector2i):
 	var id_path = astar_grid.get_id_path(
 		tile_map.local_to_map(global_position),
-		tile_map.local_to_map(destination)
+		destination
 	).slice(1)
 	
 	if id_path.is_empty() == false:
