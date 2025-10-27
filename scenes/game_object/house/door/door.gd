1679eb0d894b12c944a703fb2fa1d278fafa9640
Matt Rosanio <matt.rosanio@gmail.com>
2025-10-26 21:10:35 -0400

    Remove old house asset, add doors to be able to access interiors

---
+++ scenes/game_object/house/door/door.gd
@@ -0,0 +1,33 @@
+extends StaticBody2D
+
+@export var player: CharacterBody2D
+
+@onready var collider: CollisionShape2D = $CollisionShape2D
+@onready var sprite: Sprite2D = $Sprite2D
+
+var is_open := false
+
+
+func _ready():
+	($InteractableComponent as Interactable).interact.connect(on_interact)
+
+
+func on_interact():
+	is_open = not is_open
+	if not is_open:
+		_ensure_player_clear()
+	sprite.visible = !sprite.visible
+	collider.disabled = !collider.disabled
+
+
+func _ensure_player_clear():
+	var door_center = collider.global_position
+	var door_size = (collider.shape as RectangleShape2D).size
+	var door_position = door_center - Vector2(door_size / 2)
+	var door_rect = Rect2(door_position, door_size)
+	var player_collider_center = player.get_node("CollisionShape2D").global_position
+	if door_rect.has_point(player_collider_center):
+		if player_collider_center.y > door_center.y:
+			player.global_position.y = door_rect.end.y
+		else:
+			player.global_position.y = door_rect.position.y
