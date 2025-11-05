8078ee59ddaad35e496a4783b7c3130e2904a16c
Matt Rosanio <matt.rosanio@gmail.com>
2025-11-04 20:10:33 -0500

    Refactor interact prompt to show up on UI layer

--- scenes/components/interactable_component.gd
+++ scenes/components/interactable_component.gd
@@ -1,23 +1,32 @@
 # This component must be the direct descendant of an Area2D to work properly.
 
-extends Node
+extends Node2D
 class_name Interactable
 
 signal interact
 
+@export var interact_texture: Texture2D
+
 @onready var area: Area2D = get_parent()
-@onready var texture_rect: TextureRect = $TextureRect
+@onready var active_ui_layer: CanvasLayer = $"/root/Main/ActiveUILayer"
+
+var texture_rect: TextureRect
 var active: bool = false
 
 
 func set_active():
 	active = true
-	texture_rect.visible = true
+	texture_rect = TextureRect.new()
+	texture_rect.position = get_global_transform_with_canvas().get_origin()
+	texture_rect.scale = Vector2(2, 2)
+	texture_rect.texture = interact_texture
+	active_ui_layer.add_child(texture_rect)
 
 
 func set_inactive():
 	active = false
-	texture_rect.visible = false
+	active_ui_layer.remove_child(texture_rect)
+	texture_rect = null
 
 
 func emit_interact(interactor: Node):
